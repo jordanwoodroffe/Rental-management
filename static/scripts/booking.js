a9a80babb8576fa5df4243fb70d9eeb1c9e2ba77
Daniel Dao <guangdanny@gmail.com>
2020-05-23 19:01:42 +1000

    add transmission to filter bar

--- static/scripts/booking.js
+++ static/scripts/booking.js
@@ -1,55 +1,3 @@
-function capacityChanged(){
-  capacityVal = document.getElementById('capacity-filter').value
-  var x = document.getElementsByClassName("car");
-  for (var i = 0; i < x.length; i++) {
-    var capacity = x[i].querySelectorAll(".capacity-value")[0].innerHTML.substring(9)
-    if (parseInt(capacity) < parseInt(capacityVal)){
-      x[i].style.display = "none";
-    } else {
-      x[i].style.display = "inline-block";
-    }
-  }
-}
-
-function colorFilter(){
-  colourVal = document.getElementById('colour-filter').value
-  var x = document.getElementsByClassName("car");
-  for (var i = 0; i < x.length; i++) {
-    var colour = x[i].querySelectorAll(".colour-value")[0].innerHTML.substring(8)
-    if (colour != colourVal){
-      x[i].style.display = "none";
-    } else {
-      x[i].style.display = "inline-block";
-    }
-  }
-}
-
-function makeFilter(){
-  makeVal = document.getElementById('make-filter').value
-  var x = document.getElementsByClassName("car");
-  for (var i = 0; i < x.length; i++) {
-    var make = x[i].querySelectorAll(".make-value")[0].innerHTML
-    if (!make.includes(makeVal)){
-      x[i].style.display = "none";
-    } else {
-      x[i].style.display = "inline-block";
-    }
-  }
-}
-
-function yearFilter(){
-  yearVal = document.getElementById('year-filter').value
-  var x = document.getElementsByClassName("car");
-  for (var i = 0; i < x.length; i++) {
-    var make = x[i].querySelectorAll(".make-value")[0].innerHTML
-    if (!make.includes(yearVal)){
-      x[i].style.display = "none";
-    } else {
-      x[i].style.display = "inline-block";
-    }
-  }
-}
-
 function update_capacity_range(){
   minLabel = document.getElementById('min-capacity');
   maxLabel = document.getElementById('max-capacity');
@@ -109,15 +57,17 @@ var filter = function() {
   searchVal = document.getElementById('search-box').value
   costMax = document.getElementById('cost-filter-max').value
   costMin = document.getElementById('cost-filter-min').value
+  transmissionVal = document.getElementById('transmission-filter').value
 
   var x = document.getElementsByClassName("car");
   for (var i = 0; i < x.length; i++) {
     var make = x[i].querySelectorAll(".make-value")[0].innerHTML
     var colour = x[i].querySelectorAll(".colour-value")[0].innerHTML.substring(8)
+    var transmission = x[i].querySelectorAll(".transmission-value")[0].innerHTML
     var capacity = x[i].querySelectorAll(".capacity-value")[0].innerHTML.substring(9)
     var cost = x[i].querySelectorAll(".cost-value")[0].innerHTML.substr(7, x[i].querySelectorAll(".cost-value")[0].innerHTML.indexOf(' '))
 
-    if ((!make.toLowerCase().includes(searchVal.toLowerCase()) && searchVal != "") || (!make.includes(makeVal) && makeVal != 'All' )|| (!make.includes(yearVal) && yearVal != 'All' ) || (colour != colourVal && colourVal != 'All' ) || !((parseInt(capacity) <= parseInt(capacityMax)) && (parseInt(capacity) >= parseInt(capacityMin))) || !((parseInt(cost) <= parseInt(costMax)) && (parseInt(cost) >= parseInt(costMin))) ){
+    if ((transmission != transmissionVal && transmissionVal != 'All' ) || (!make.toLowerCase().includes(searchVal.toLowerCase()) && searchVal != "") || (!make.includes(makeVal) && makeVal != 'All' )|| (!make.includes(yearVal) && yearVal != 'All' ) || (colour != colourVal && colourVal != 'All' ) || !((parseInt(capacity) <= parseInt(capacityMax)) && (parseInt(capacity) >= parseInt(capacityMin))) || !((parseInt(cost) <= parseInt(costMax)) && (parseInt(cost) >= parseInt(costMin))) ){
       x[i].style.display = "none";
     } else {
       x[i].style.display = "inline-block";
