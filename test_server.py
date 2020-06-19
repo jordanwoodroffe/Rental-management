c69438bf6d79a46b5c852c37a054651205afac03
Evan Mason <evzyman@hotmail.com>
2020-06-19 20:44:58 +1000

    tests work!

--- test_server.py
+++ test_server.py
@@ -4,10 +4,10 @@ import server
 class TestServer(unittest.TestCase):
 
     def test_unlockCar(self):
-        self.assertTrue(server.unlockCar("_unloCar" + "VSB296" + "_user_" + "jwoodroffe"))
+        self.assertTrue(server.unlockCar("Message from Client:b'_unloCarVSB296_user_jwoodroffe'"))
 
     def test_lockCar(self):
-        self.assertTrue(server.lockCar("_unloCar" + "VSB296" + "_user_" + "jwoodroffe"))
+        self.assertTrue(server.lockCar("Message from Client:b'_lockedCarVSB296_user_jwoodroffe'"))
 
 if __name__ == '__main__':
     unittest.main()
